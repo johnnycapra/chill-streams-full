1c108d3df1a443bc4bebcdf9d83927b569dbd2dd
itsjives <jonathanwcapra@gmail.com>
2020-07-06 03:09:03 -0700

    created modal, wired up editing

--- streams-client/src/components/streams/StreamCreate.js
+++ streams-client/src/components/streams/StreamCreate.js
@@ -1,7 +1,22 @@
 import React from 'react';
+import { createStream } from '../../actions';
+import { connect } from 'react-redux';
+import StreamForm from './StreamForm';
 
-const StreamCreate = () => {
-    return <div>StreamCreate</div>
-}
+class StreamCreate extends React.Component {
 
-export default StreamCreate; 
+    onSubmit = (formValues) => {
+        this.props.createStream(formValues);
+    };
+
+    render() {
+        return (
+            <div>
+                <h3>Create a stream.</h3>
+                <StreamForm onSubmit={this.onSubmit} />
+            </div>
+        );
+    };
+};
+
+export default connect(null, { createStream })(StreamCreate);
